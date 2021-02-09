d403e9064304e399e9b5721bd9a2c133f85ab5ac
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-02-09 10:38:31 -0500

    create simple Exercise model (id/exercise_bool)

--- models/Exercise.js
+++ models/Exercise.js
@@ -1,12 +1,27 @@
 const { Model, DataTypes } = require("sequelize");
-const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
 
 class Exercise extends Model {}
 
 Exercise.init (
     {
-
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        exercise_bool: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
     },
     {
     sequelize,
