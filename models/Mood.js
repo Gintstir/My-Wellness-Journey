6bc709d9e5870381adaeb2fd4ad5fa4158a18843
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-02-09 10:05:55 -0500

    added Mood model(id/title/mood_rating/mood_post)

---
+++ models/Mood.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//create mood model
+class Mood extends Model {}
+
+Mood.init (
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allownull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        mood_rating: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        mood_post: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [10]
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'mood'
+    }
+);
+
+module.exports = Mood;
--- models/Post.js
+++ models/Post.js
@@ -6,7 +6,7 @@ class Post extends Model {}
 
 Post.init (
     {
-
+        
     },
     {
     sequelize,
