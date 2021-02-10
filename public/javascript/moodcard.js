d850594e612e47c6456e288a1983b61cfcaa9009
jyc5331 <jcosgrovecoding@gmail.com>
2021-02-09 19:25:22 -0500

    validation added for myFunction in moodcard.js

--- public/javascript/moodcard.js
+++ public/javascript/moodcard.js
@@ -1,10 +1,14 @@
 const moodButton = document.querySelector("#moodButton");
 
 function myFunction() {
-  var moodInput = document.querySelector("#moodSlider").value;
-  console.log(moodInput);
   var titleInput = document.querySelector("#moodTitle").value;
   console.log(titleInput);
+  if ((titleInput = "Enter your mood")) {
+    alert("Please enter a mood");
+    return;
+  }
+  var moodInput = document.querySelector("#moodSlider").value;
+  console.log(moodInput);
   var moodDate = document.querySelector("#moodDate").value;
   console.log(moodDate);
 }
